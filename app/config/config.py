03019200d3119a3bb38cda1c314830c713cf5d8a
Adriano V. dos Santos <adriano.santos@serpro.gov.br>
2019-12-23 14:35:59 -0300

    Parâmetros de configuração.

---
+++ app/config/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_port = os.getenv("DB_PORT")
+db_schema = os.getenv("DB_SCHEMA")
